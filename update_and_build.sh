e7d7df87dc3a419cabaadd57a2f723d26a5b79ad
Mohamdadan <149867728+Mohamdadan@users.noreply.github.com>
2024-08-12 17:50:06 +0300

    Update update_and_build.sh

--- update_and_build.sh
+++ update_and_build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 image="mohamedadan4323/pro1:v6"
+containername="LMS"
 # Navigate to the repository directory
 cd /root/repos/html || { echo "Failed to change directory to /root/repos/html"; exit 1; }
 
@@ -10,6 +11,6 @@ git pull origin || { echo "Failed to pull from origin"; exit 1; }
 # Build the Docker image
 docker build -t $image .  
 
-docker run -d -p 80:80 --name $image   $image
+docker run -d -p 80:80 --name $containername   $image
 
 echo "Script completed successfully"
