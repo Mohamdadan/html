2556e27ee8cdda183bbf451588ada71058597228
Mohamdadan <149867728+Mohamdadan@users.noreply.github.com>
2024-08-12 16:56:31 +0300

    Update update_and_build.sh

--- update_and_build.sh
+++ update_and_build.sh
@@ -8,6 +8,6 @@ git fetch origin || { echo "Failed to fetch from origin"; exit 1; }
 git pull origin || { echo "Failed to pull from origin"; exit 1; }
 
 # Build the Docker image
-docker build -t mohamedadan4323/pro1:v1 . || { echo "Failed to build Docker image"; exit 1; }
+docker build -t mohamedadan4323/pro1:v2 . || { echo "Failed to build Docker image"; exit 1; }
 
 echo "Script completed successfully"
