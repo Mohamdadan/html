444f8288b47c7d2f8afa79c8266780019858b42d
Mohamdadan <149867728+Mohamdadan@users.noreply.github.com>
2024-08-12 17:51:41 +0300

    Update update_and_build.sh

--- update_and_build.sh
+++ update_and_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-image="mohamedadan4323/pro1:v6"
+image="mohamedadan4323/pro1:v6.1"
 containername="LMS"
 # Navigate to the repository directory
 cd /root/repos/html || { echo "Failed to change directory to /root/repos/html"; exit 1; }
